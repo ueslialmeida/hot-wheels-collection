b95415b195934eebc5edfaefa35d5dbd93335d46
Uesli Almeida <almeida.uesli@gmail.com>
2026-05-15 13:33:01 -0300

    Updates tests.

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('/')
+    cy.visit('http://localhost:3000')
     cy.get('h2').should('contain.text', 'Bem-vindo de volta')
   })
 })
